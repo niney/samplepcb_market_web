607137b0a8a7e272908297de9e0d981c8092e5a2
Aosi <mania001@nate.com>
2021-03-12 14:09:56 +0900

    react router dom

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,8 +1,22 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client"
+
+export const isLoggedInVar = makeVar(false)
 
 export const client = new ApolloClient({
   uri: "https://api-samplepcbmarket.herokuapp.com/graphql",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          isLoggedIn: {
+            read() {
+              return isLoggedInVar()
+            },
+          },
+        },
+      },
+    },
+  }),
 })
 
 export default client
