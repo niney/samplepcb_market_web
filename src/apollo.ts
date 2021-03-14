cd0bbaf8b1612f3b1ca694933309307c0295abc1
Aosi <mania001@nate.com>
2021-03-14 22:00:29 +0900

    Using the Token

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,12 +1,31 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client"
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
 import { LOCALSTORAGE_TOKEN } from "./constants"
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN)
 export const isLoggedInVar = makeVar(Boolean(token))
 export const authTokenVar = makeVar(token)
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "https://api-samplepcbmarket.herokuapp.com/graphql",
+})
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${authTokenVar()}` || "",
+    },
+  }
+})
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
